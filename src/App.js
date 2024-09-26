13606ebff2ea83122383ade99886d5d796d87686
Shuchi Yadav <shuchiyadav2307@gmail.com>
2024-09-26 12:30:10 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+//const App3 = () => {
+  //fetch('https://reqres.in/api/users?page=2')
+  //.then((response)=>{
+    //  return response.json()
+  //})
+  //.then((response)=>{
+    //  console.log(response.data)
+  //})
+
+  //return <h1>Check console</h1>
+//}
+
+//export default App3
+
+//const App4 = () => {
+  //useEffect(()=> {
+    //fetch('https://reqres.in/api/users?page=2')
+    //.then((response)=>{
+      //  return response.json()
+    //})
+    //.then((response) => {
+        //console.log(response.data)
+    //})
+  //},[])
+    //return <h1>Check Console</h1>
+//}
+
+//export default App4
+
+const App3 = () => {
+  const [user, setUser] = useState(null)
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        let response = await fetch('https://reqres.in/api/users/2')
+        let result = await response.json()
+        setUser(result.data) 
+      }catch(error){
+        console.error('Error fetching', error)
+      }
+    };
+    fetchUser()
+  },[])
+
+  return(
+    <div>
+      <h1>{user?.first_name}</h1>{/*Adjust to use correct key from API */}
     </div>
-  );
+  )
 }
-
-export default App;
+export default App3
